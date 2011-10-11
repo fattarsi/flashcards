ed957cc9b263f22fd66d8b7df57ca943a8ccf7a6
Chris Fattarsi <cfattarsi@gmail.com>
2011-10-11 18:07:18 -0500

    large re-structure of javascript

--- controller.js
+++ controller.js
@@ -11,9 +11,248 @@ if (!Modernizr.localstorage) {
   setMsg('Your browser does not support cool features of HTML5 like localstorage, therefore cannot use this app.');
 }
 
-function initDeck() {
-  var DECK = new Deck('deck');
+//show add form
+function add() {
+  hotkeyDisable();
+  document.getElementById('key').value = '';
+  document.getElementById('button-save').onclick = save;
+  resetDisplay();
+  show('phrase-form');
+  document.getElementById('phrase-1').focus();
+}
+
+//reset form fields
+function cancel() {
+  document.getElementById('phrase-1').value = '';
+  document.getElementById('phrase-2').value = '';
+  hotkeyEnable();
+  resetDisplay();
+  show('card-container');
   updateDisplay();
+}
+
+//do action for a given hotkey
+function checkHotkey(e) {
+  var key = (window.event) ? event : e;
+  //alert(key.keyCode);
+  switch(key.keyCode) {
+    case 32:
+      flip();
+      break;
+    case 37:
+      prev();
+      break;
+    case 39:
+      next();
+      break;
+    case 40:
+    case 38:
+      flip();
+      break;
+  }
+}
+
+function del() {
+  document.getElementById('conf-msg').innerHTML = 'Are you sure you want to delete this card?';
+  document.getElementById('conf-yes').onclick = delYes;
+  document.getElementById('conf-no').onclick = delNo;
+  hide('msg-container');
+  show('conf');
+}
+
+function delNo() {
+  setMsg('Delete canceled');
+  hide('conf');
+}
+
+function delYes() {
+    DECK.deleteCard();
+    updateDisplay();
+    hide('conf');
+}
+
+//set form to edit the current card
+function edit() {
+  var card = DECK.current();
+  hotkeyDisable();
+  resetDisplay();
+  document.getElementById('phrase-1').value = card.phrase1;
+  document.getElementById('phrase-2').value = card.phrase2;
+  document.getElementById('key').value = card.key;
+  show('phrase-form');
+  document.getElementById('phrase-1').focus();
+}
+
+//display alternate phrase
+function flip() {
+    toggle('main');
+    toggle('main-alt');
+}
+
+function flipReset() {
+    if (DECK.mode_reverse) {
+        document.getElementById('main').style.display = 'none';
+        document.getElementById('main-alt').style.display = '';
+    } else {
+        document.getElementById('main').style.display = '';
+        document.getElementById('main-alt').style.display = 'none';
+    }
+}
+
+function hide(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+function hotkeyDisable() {
+  document.onkeydown = null;
+}
+
+function hotkeyEnable() {
+  document.onkeydown = checkHotkey;
+}
+
+function initDeck() {
+  DECK = new Deck('deck');
+  //if deck is empty it could be first run or need to be migrated
+  if (DECK.length() <= 0) {
+    migrationCheck();
+  }
+  updateDisplay();
+}
+
+//migrate a previous schema to current if needed
+function migrationCheck() {
+    var c = localStorage["cards"];
+    if (c) {
+        var cards = JSON.parse(localStorage["cards"]);
+        for (var ndx=0; ndx < cards.length; ndx++) {
+            var oldCard = JSON.parse(localStorage[cards[ndx]]);
+            var newCard = new Card({'phrase1':oldCard['1'], 'phrase2':oldCard['2'], 'points':oldCard['points']});
+            newCard.save();
+            DECK.add(newCard);
+            localStorage.removeItem(cards[ndx]);
+        }
+        DECK.save();
+        localStorage.removeItem("cards");
+    }
+}
+
+function msgClose() {
+  document.getElementById('msg-container').style.display = 'none';
+}
+
+function navShow(){
+  show('bottom-panel');
+  show('button-delete');
+  show('button-edit');
+  show('meter');
+  show('options-container');
+  show('stats');
+}
+
+function navHide() {
+  hide('add-another');
+  hide('bottom-panel');
+  hide('button-delete');
+  hide('button-edit');
+  hide('meter');
+  hide('options-container');
+  hide('stats');
+}
+
+//display next card
+function next() {
+    DECK.next();
+    updateDisplay();
+}
+
+//adjust point of card
+function pointDown() {
+    var card = DECK.current();
+    card.pointDown();
+    card.save();
+    next();
+}
+
+function pointUp() {
+    var card = DECK.current();
+    card.pointUp();
+    card.save();
+    next();
+}
+
+//display previous card
+function prev() {
+    DECK.prev();
+    updateDisplay();
+}
+
+function reset() {
+  document.getElementById('conf-msg').innerHTML = 'Are you sure you want to reset everything?';
+  document.getElementById('conf-yes').onclick = resetYes;
+  document.getElementById('conf-no').onclick = resetNo;
+  hide('msg-container');
+  show('conf');
+  document.getElementById('conf-no').focus();
+}
+
+//clear all blocks that may be showing in the main display
+function resetDisplay() {
+  hide('add-another');
+  hide('card-container');
+  hide('phrase-form');
+}
+
+function resetYes() {
+  localStorage.clear();
+  setMsg('Reset settings');
+  initDeck();
+  hide('conf');
+}
+
+function resetNo() {
+  setMsg('Reset canceled');
+  hide('conf');
+}
+
+function save() {
+  var phrase1 = document.getElementById('phrase-1').value;
+  var phrase2 = document.getElementById('phrase-2').value;
+  
+  if (!phrase1 || !phrase2) {
+    setMsg('Both fields are required');
+    return;
+  }
+  
+  var key = document.getElementById('key').value;
+  var card;
+  var msg;
+  //key is set -> edit
+  if (key) {
+    card = new Card({'key':key});
+    card.phrase1 = phrase1;
+    card.phrase2 = phrase2;
+    card.save();
+    msg = 'Card updated';
+  } else {
+    card = new Card({'phrase1':phrase1,'phrase2':phrase2});
+    card.save();
+    DECK.add(card);
+    DECK.save();
+    msg = 'Card added';
+  }
+  
+  setMsg(msg);
+  cancel();
+  updateDisplay();
+  resetDisplay();
+  show('add-another');
+  document.getElementById('button-add-another').focus();
+  hotkeyEnable();
+}
+
+function show(id) {
+  document.getElementById(id).style.display = '';
 }
 
 //show a message dialog
@@ -23,6 +262,81 @@ function setMsg(msg) {
   show('msg-container');
 }
 
+function setStats(msg) {
+  document.getElementById('stats').innerHTML = msg;
+}
+
+//toggle visibility of an id
+function toggle(id) {
+  if (document.getElementById(id).style.display == 'none') {
+    document.getElementById(id).style.display = '';
+  } else {
+    document.getElementById(id).style.display = 'none';
+  }
+}
+
+function toggleOption(elm) {
+    //find option and do work
+    switch(elm.id) {
+      case 'lows':
+        DECK.toggleLow();
+        break;
+      case 'highs':
+        DECK.toggleHigh();
+        break;
+      case 'random':
+        DECK.toggleRandom();
+        break;
+      case 'reverse':
+        DECK.toggleReverse();
+        break;
+    }
+  updateDisplay();
+}
+
+//show/hide the options
+function toggleOptionsShow() {
+  toggle('options');
+  //change container class to show state
+  var elm = document.getElementById('options-container');
+  if (elm.className == 'on') {
+    elm.className = 'off';
+  } else {
+    elm.className = 'on';
+  }
+}
+
+//set display for the current card
 function updateDisplay() {
+    flipReset();
+    hide('conf');
+    var card = DECK.current();
+    if (!card) {
+        // set help text for first run.
+        navHide();
+        setMsg('Click on messages to close.');
+        document.getElementById('main').innerHTML = 'Click here to toggle';
+        document.getElementById('main-alt').innerHTML = 'Now add some';
+        document.getElementById('button-add').focus();
+        show('card-container');
+    } else {
+        navShow();
+        document.getElementById('main').innerHTML = card.phrase1;
+        document.getElementById('main-alt').innerHTML = card.phrase2;
+        document.getElementById('meter').innerHTML = card.points;
+        document.getElementById('key').value = card.key;
+        
+        setStats((DECK.index+1) + '/' + DECK.length());
+    }
     
+    updateOptions();
+    
+}
+
+//update the state of the options to show current state
+function updateOptions() {
+    document.getElementById('highs').className = (DECK.mode_high) ? 'on' : 'off';
+    document.getElementById('lows').className = (DECK.mode_low) ? 'on' : 'off';
+    document.getElementById('random').className = (DECK.mode_random) ? 'on' : 'off';
+    document.getElementById('reverse').className = (DECK.mode_reverse) ? 'on' : 'off';
 }
--- deck.js
+++ deck.js
@@ -8,14 +8,24 @@
 function Deck(key) {
     this.key = key;
     var d = (localStorage[key]) ? JSON.parse(localStorage[key]) : {} ;
-    this.index = (d.index) ? d.index : 0;
-    this.cards = (d.cards) ? d.cards : new Array();
+    //this.index = (d.index) ? d.index : 0;
+    this.index = 0;
+    //master_set contains all cards in order that they were added
+    this.master_set = (d.master_set) ? d.master_set : new Array();
+    //this.cards = (d.cards) ? d.cards : new Array();
+    //cards is the current ordering based on mutation options (random, low/high)
+    this.cards = this.master_set.slice();
+    this.mode_low = false;
+    this.mode_high = false;
     this.mode_random = (d.mode_random) ? d.mode_random : false;
     this.mode_reverse = (d.mode_reverse) ? d.mode_reverse : false;
+    
+    this.processOptions();
 }
 
 //add card to back of deck
 Deck.prototype.add = function (card) {
+    this.master_set.push(card.key);
     this.cards.push(card.key);
 }
 
@@ -47,8 +57,13 @@ Deck.prototype.deleteCard = function () {
     if (this.length() <= 0) {
         return;
     }
+    //rm from storage
     localStorage.removeItem(this.currentKey());
+    
+    //rm from index arrays
+    this.master_set.splice(this.master_set.indexOf(this.currentKey()),1);
     this.cards.splice(this.index, 1);
+    this.save();
 }
 
 //return number of cards in deck
@@ -58,26 +73,87 @@ Deck.prototype.length = function  () {
 
 //return the next card and advance the index
 Deck.prototype.next = function () {
-  this.index +=1;
-  if (this.index >= this.length()) {
-    this.index = 0;
-  }
+    this.index +=1;
+    if (this.index >= this.length()) {
+      this.index = 0;
+    }
   
-  return this.current();
+    return this.current();
 }
 
 //return the previous card and update the index
 Deck.prototype.prev = function () {
-  this.index -= 1;
-  if (this.index < 0) {
-    this.index = this.length() - 1;
-  }
+    this.index -= 1;
+    if (this.index < 0) {
+      this.index = this.length() - 1;
+    }
   
-  return this.current();
+    return this.current();
+}
+
+//set the cards array based on mode_* options
+Deck.prototype.processOptions = function () {
+    //sanity check that both high and low are not set, if so set none
+    if (this.mode_high && this.mode_low) {
+        this.mode_high = this.mode_low = false;
+    }
+    
+    this.cards = this.master_set.slice();
+    
+    if (this.mode_high || this.mode_low) {
+        this.cards = new Array();
+        for (var i=0; i<this.master_set.length; i++) {
+            var c = new Card({'key':this.master_set[i]});
+            if (this.mode_high && (c.points > 0)) {
+                this.cards.push(c.key);
+            } else if (this.mode_low && c.points <= 0) {
+                this.cards.push(c.key);
+            }
+        }
+        
+        //if none were returned, turn option off and return master_set
+        if (this.cards.length <= 0) {
+            this.cards = this.master_set.slice();
+            this.mode_high = false;
+            this.mode_low = false;
+        }
+    }
+    
+    if (this.mode_random) {
+        this.cards.sort(function() {return 0.5 - Math.random()});
+    }
 }
 
 //save this deck in localstorage
 Deck.prototype.save = function () {
-  localStorage[this.key] = JSON.stringify(this,null,2);
+    localStorage[this.key] = JSON.stringify(this,null,2);
+}
+
+//toggle the mode_high option
+//setting high will unset low
+Deck.prototype.toggleHigh = function () {
+    this.mode_high = (this.mode_high) ? false : true;
+    this.mode_low = (this.mode_high) ? false: this.mode_low;
+    this.processOptions();
+}
+
+//toggle the mode_low option
+//setting low will unset high
+Deck.prototype.toggleLow = function () {
+    this.mode_low = (this.mode_low) ? false : true;
+    this.mode_high = (this.mode_low) ? false: this.mode_high;
+    this.processOptions();
+}
+
+//toggle the mode_random option
+Deck.prototype.toggleRandom = function () {
+    this.mode_random = (this.mode_random) ? false : true;
+    this.processOptions();
+}
+
+//toggle the mode_reverse option
+Deck.prototype.toggleReverse = function () {
+    this.mode_reverse = (this.mode_reverse) ? false : true;
+    this.processOptions();
 }
 
